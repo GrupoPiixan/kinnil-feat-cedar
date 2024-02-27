8e34246dc8f30d26f19fc17493dd78f117dd95c0
manghidev <31641216+manghidev@users.noreply.github.com>
2024-02-27 12:14:17 -0600

    fix: update API and SocketIO URLs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,8 +13,8 @@ export const environment = {
     appId: "1:212415381093:web:8977bcb7cdd2d2d95b908c",
     measurementId: "G-CGQVNQPGS9",
   },
-  urlAPIServer: "http://68.183.26.55:8080/api",
-  urlSocketIO: "http://68.183.26.55:3005",
+  urlAPIServer: "https://back.kinnil.com:8080/api",
+  urlSocketIO: "https://back.kinnil.com:3005",
 };
 
 /*
