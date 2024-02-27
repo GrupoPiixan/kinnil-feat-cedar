52ca24356197a547707156dca075b6eac2b6361a
manghidev <31641216+manghidev@users.noreply.github.com>
2024-02-27 11:50:47 -0600

    fix: add CORS headers to serverCtrl.js

--- server/src/controllers/serverCtrl.js
+++ server/src/controllers/serverCtrl.js
@@ -78,6 +78,11 @@ class Server {
         this.app.use(cors());
         // * Lectura y parseo del body
         this.app.use(express.json());
+        app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*")
+            res.header("Access-Control-Allow-Headers", "Authorization")
+            next()
+        });
     }
 }
 
